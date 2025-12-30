4c9fe401c76d81102624fa2e9df5fce11621c369
Eunsung <hiddenjade2021@gmail.com>
2025-12-31 05:10:23 +0900

    무기 데이터 기본설명, 스킬설명 보완하고 무기 관련 ui들 전부 조정

--- Assets/01.Script/MVC/UI/WeaponSlotUIController.cs
+++ Assets/01.Script/MVC/UI/WeaponSlotUIController.cs
@@ -96,7 +96,7 @@ public class WeaponSlotUIController : MonoBehaviour
         if (textList.Count > 0)
         {
             // 첫 번째 텍스트는 description
-            textList[0].text = weaponData.weaponName;
+            textList[0].text = "< " + weaponData.weaponName + " >";
             
             // 두 번째 텍스트는 detailDescription (존재하는 경우)
             if (textList.Count > 1)
